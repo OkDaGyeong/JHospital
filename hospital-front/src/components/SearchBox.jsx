6aed1a5108cfd11ca4a97b1eaef860aae77f2a8e
OkDaGyeong <81674153+OkDaGyeong@users.noreply.github.com>
2023-07-25 02:57:33 +0900

    .

--- hospital-front/src/components/SearchBox.jsx
+++ hospital-front/src/components/SearchBox.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  Form,
-  Button,
-  InputGroup,
-  Stack,
-  FloatingLabel,
-} from "react-bootstrap";
+import { Form, Button, Stack } from "react-bootstrap";
 import "../styles/patientListPage.scss";
 
 function SearchBox() {
